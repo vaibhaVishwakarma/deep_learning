5625d9f47ebbf2057cbd8da0299deff88ccebf94
vaibhaVishwakarma <164744837+vaibhaVishwakarma@users.noreply.github.com>
2024-06-11 20:57:52 +0530

    Update M1_functions.py

--- M1_functions.py
+++ M1_functions.py
@@ -32,7 +32,7 @@ def train(model:nn.Module = None, loss_fn =loss_fn ,lr = 0.1,epochs = 3, trainDa
 
 
 
-def show_grid(row:int = 5, col:int = 5  ,test_data:torch.utils.data.datasets=test_data , Cnn:int = 0 ,  model:nn.Module):
+def show_grid(model:nn.Module =None, row:int = 5, col:int = 5  ,test_data:torch.utils.data.datasets=test_data , Cnn:int = 0 ):
 """
 def pred(self , X):
     self.eval()
